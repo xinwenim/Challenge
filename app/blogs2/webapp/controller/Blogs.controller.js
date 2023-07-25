9f977b2e217134879962d387f00565236387855e
WENXIN <lana.wen@sap.com>
2023-07-25 06:59:48 +0000

    add app blogs and modified tbale

---
+++ app/blogs2/webapp/controller/Blogs.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("blogs2.controller.Blogs", {
+            onInit: function () {
+
+            }
+        });
+    });
